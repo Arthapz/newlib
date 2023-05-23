25e915f5d416a0773f2cd1425dfbfe864ef50803
Arthur LAURENT <arthur.laurent4@gmail.com>
2023-05-23 18:56:38 +0200

    fix missing include dir

--- xmake.lua
+++ xmake.lua
@@ -7,8 +7,8 @@ local flags = {
 add_cxflags(table.unwrap(flags))
 add_asflags(table.unwrap(flags))
 
-add_sysincludedirs("libc/machine/aarch64/")
-add_sysincludedirs("libc/machine/aarch64/sys")
+add_sysincludedirs("newlib/libc/machine/aarch64/")
+add_sysincludedirs("newlib/libc/machine/aarch64/sys")
 add_sysincludedirs("newlib/libc/sys/arm")
 add_sysincludedirs("newlib/libc/include")
 
